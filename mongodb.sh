61e04f5a396136d8fe8e66acac8b623518d963fb
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-08-22 11:48:50 +0530

    Mongo headings

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,16 @@
+echo -e "\e[34m>>>>>>> Configure mongo repo >>>>>>>\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[34m>>>>>>> Install Mongo software  >>>>>>>\e[0m"
 yum install mongodb-org -y
+
+echo -e "\e[34m>>>>>>> enable and start mongod  >>>>>>>\e[0m"
 systemctl enable mongod
 systemctl start mongod
+
+echo -e "\e[34m>>>>>>> Update listen address  >>>>>>>\e[0m"
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
-#edit the file and replace 127.0.0.1 to 0.0.0.0
+#edit the file and replace 127.0.0.1 to 0.0.0.
+
+echo -e "\e[34m>>>>>>> Restart mongo  >>>>>>>\e[0m"
 systemctl restart mongod
