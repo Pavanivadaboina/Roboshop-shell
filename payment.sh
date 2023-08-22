91d6ea0fbf2046ecba98e591a9ce85e952a45a3f
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-08-22 15:36:11 +0530

    shipping and RabbitMQ headings

--- payment.sh
+++ payment.sh
@@ -1,12 +1,28 @@
+echo -e "\e[34m>>>>>>> Install Python <<<<<<<\e[0m"
 yum install python36 gcc python3-devel -y
+
+echo -e "\e[34m>>>>>>> Add Application user  <<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[34m>>>>>>> Create App Direactory <<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
+
+echo -e "\e[34m>>>>>>> Download the application content <<<<<<<\e[0m"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
+
+echo -e "\e[34m>>>>>>> Extract the application Content <<<<<<<\e[0m"
 cd /app
 unzip /tmp/payment.zip
+
+echo -e "\e[34m>>>>>>> Install Python Dependencies <<<<<<<\e[0m"
 cd /app
 pip3.6 install -r requirements.txt
-cp payment.service /etc/systemd/system/payment.service
+
+echo -e "\e[34m>>>>>>> Setup SystemD Payment service <<<<<<<\e[0m"
+cp /home/centos/Roboshop-shell/payment.service /etc/systemd/system/payment.service
+
+echo -e "\e[34m>>>>>>> load and restart the service <<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable payment
 systemctl start payment
--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,7 +1,16 @@
+echo -e "\e[34m>>>>>>> Setup Erlang repos <<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+
+echo -e "\e[34m>>>>>>> Setup RabbitMQ Repos <<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+
+echo -e "\e[34m>>>>>>> Install RabbitMQ <<<<<<<\e[0m"
 yum install rabbitmq-server -y
+
+echo -e "\e[34m>>>>>>> enable and start RabbitMQ <<<<<<<\e[0m"
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
+
+echo -e "\e[34m>>>>>>> Create Application User in RabbitMQ <<<<<<<\e[0m"
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
