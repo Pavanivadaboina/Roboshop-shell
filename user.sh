8927d0590c65282fd560c4834c8cc23d411b44c6
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-08-24 13:57:25 +0530

    cart and common.sh code dry

--- user.sh
+++ user.sh
@@ -2,38 +2,9 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source $(script_path)/common.sh
 
-echo -e "\e[34m>>>>>>> Configure NodeJS repos >>>>>>>\e[0m"
+component=user
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-
-echo -e "\e[34m>>>>>>> Install NodeJS >>>>>>>\e[0m"
-yum install nodejs -y
-
-echo -e "\e[34m>>>>>>> Add Application User  >>>>>>>\e[0m"
-useradd ${app_user}
-
-echo -e "\e[34m>>>>>>> Create App Directory >>>>>>>\e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[34m>>>>>>> Download the Application content >>>>>>>\e[0m"
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-
-echo -e "\e[34m>>>>>>> Unzip the app content >>>>>>>\e[0m"
-cd /app
-unzip /tmp/user.zip
-
-echo -e "\e[34m>>>>>>> Install Dependencies  >>>>>>>\e[0m"
-npm install
-
-echo -e "\e[34m>>>>>>> Setup SystemD Service   >>>>>>>\e[0m"
-cp ${script_path}/user.service /etc/systemd/system/user.service
-
-echo -e "\e[34m>>>>>>> Start User Service >>>>>>>\e[0m"
-systemctl daemon-reload
-systemctl enable user
-systemctl restart user
-
+func_nodejs
 
 echo -e "\e[34m>>>>>>> copy mongo repo    >>>>>>>\e[0m"
 cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
