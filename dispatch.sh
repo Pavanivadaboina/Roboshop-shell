284d38f6d972710ba7de196c869fc20d704cb482
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-08-26 23:05:03 +0530

    cart and common.sh code dry

--- dispatch.sh
+++ dispatch.sh
@@ -3,16 +3,8 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
 yum install golang -y
-useradd ${app_user}
-mkdir /app
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
-cd /app
-unzip /tmp/dispatch.zip
-cd /app
+func_app_prereq
 go mod init dispatch
 go get
 go build
-cp ${script_path}/dispatch.service /etc/systemd/system/dispatch.service
-systemctl daemon-reload
-systemctl enable dispatch
-systemctl start dispatch
+func_systemd
