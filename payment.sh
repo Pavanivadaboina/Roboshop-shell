369901310bb676539ccb57a9b0d05097ab231564
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-08-29 15:23:37 +0530

    cart and common.sh code dry

--- payment.sh
+++ payment.sh
@@ -11,32 +11,25 @@ then
   exit
 fi
 
-echo -e "\e[34m>>>>>>> Install Python <<<<<<<\e[0m"
-yum install python36 gcc python3-devel -y
 
-echo -e "\e[34m>>>>>>> Add Application user  <<<<<<<\e[0m"
-useradd ${user_app}
+print_head "Install Python"
+yum install python36 gcc python3-devel -y &>>log_file
+func_status_check $?
 
-echo -e "\e[34m>>>>>>> Create App Directory <<<<<<<\e[0m"
-rm -rf /app
-mkdir /app
+func_app_prereq()
 
-echo -e "\e[34m>>>>>>> Download the application content <<<<<<<\e[0m"
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
+print_head "Install Python Dependencies"
+cd /app &>>$log_file
+pip3.6 install -r requirements.txt &>>$log_file
+func_status_check $?
 
-echo -e "\e[34m>>>>>>> Extract the application Content <<<<<<<\e[0m"
-cd /app
-unzip /tmp/payment.zip
+print_head  "Setup SystemD Payment service"
+sed -i -e 's|rabbitmq_user_password|${rabbitmq_user_password}|' ${script_path}/payment.service &>>$log_file
+cp ${script_path}/payment.service /etc/systemd/system/payment.service &>>$log_file
+func_status_check $?
 
-echo -e "\e[34m>>>>>>> Install Python Dependencies <<<<<<<\e[0m"
-cd /app
-pip3.6 install -r requirements.txt
-
-echo -e "\e[34m>>>>>>> Setup SystemD Payment service <<<<<<<\e[0m"
-sed -i -e 's|rabbitmq_user_password|${rabbitmq_user_password}|' ${script_path}/payment.service
-cp ${script_path}/payment.service /etc/systemd/system/payment.service
-
-echo -e "\e[34m>>>>>>> load and restart the service <<<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable payment
-systemctl start payment
+print_head "load and restart"
+systemctl daemon-reload &>>$log_file
+systemctl enable payment &>>$log_file
+systemctl start payment &>>$log_file
+func_status_check $?
