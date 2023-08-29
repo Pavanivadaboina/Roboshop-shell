62a87ec594831bf2ead99ad46fd6466f10ded81f
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-08-29 14:49:59 +0530

    cart and common.sh code dry

--- mongodb.sh
+++ mongodb.sh
@@ -2,19 +2,26 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-echo -e "\e[34m>>>>>>> Configure mongo repo >>>>>>>\e[0m"
+
+
+print_head "Configure mongo repo"
 cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+func_status_check $?
 
-echo -e "\e[34m>>>>>>> Install Mongo software  >>>>>>>\e[0m"
+print_head "Install Mongo software"
 yum install mongodb-org -y
+func_status_check $?
 
-echo -e "\e[34m>>>>>>> enable and start mongod  >>>>>>>\e[0m"
+print_head "Start mongod"
 systemctl enable mongod
 systemctl start mongod
+func_status_check $?
 
-echo -e "\e[34m>>>>>>> Update listen address  >>>>>>>\e[0m"
+print_head " replace the IP Address "
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 #edit the file and replace 127.0.0.1 to 0.0.0.
+func_status_check $?
 
-echo -e "\e[34m>>>>>>> Restart mongo  >>>>>>>\e[0m"
+print_head "restart mongod "
 systemctl restart mongod
+func_status_check $?
