e84342762afa7623941e73465447bcbe06a4d59c
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-08-29 14:38:38 +0530

    cart and common.sh code dry

--- frontend.sh
+++ frontend.sh
@@ -2,26 +2,35 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-echo -e "\e[32m>>>>>>> Inatsll NGINIX >>>>>>>\e[0m"
-yum install nginx -y
+component=roboshop
 
-echo -e "\e[32m>>>>>>> Start Nginx >>>>>>>\e[0m"
-systemctl enable nginx
-systemctl start nginx
 
-echo -e "\e[32>>>>>>> remove default code <<<<<<<\e[0m"
-rm -rf /usr/share/nginx/html/*
+print_head " install Nginx "
+yum install nginx -y &>>$log_file
+func_status_check $?
 
-echo -e "\e[32m>>>>>>> Download the App Content <<<<<<<\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+print_head " System start nginx "
+systemctl enable nginx &>>$log_file
+systemctl start nginx  &>>$log_file
+func_status_check $?
 
-echo -e "\e[32m >>>>>>> Unzip App Content <<<<<<<\e[0m"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+print_head " remove default versions "
+rm -rf /usr/share/nginx/html/*  &>>$log_file
+func_status_check $?
 
-echo -e "\e[32m >>>>>>> Set up SystemD Service <<<<<<<\e[0m"
-cp ${script_path}/roboshop.conf /etc/nginx/default.d/roboshop.conf
+print_head "Download the application content "
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+func_status_check $?
 
-echo -e "\e[32m >>>>>>> Restart Nginx <<<<<<<\e[0m"
-systemctl restart nginx
+print_head " unzip the application content "
+cd /usr/share/nginx/html &>>$log_file
+unzip /tmp/frontend.zip &>>$log_file
+func_status_check $?
 
+print_head " setup systemD service "
+cp ${script_path}/${component}.conf /etc/nginx/default.d/${component}.conf &>>$log_file
+func_status_check $?
+
+print_head " restart nginx "
+systemctl restart nginx &>>$log_file
+func_status_check $?
