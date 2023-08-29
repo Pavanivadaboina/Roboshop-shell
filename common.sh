3b9f18cd03180e2d5d574cd48840e92bbd224165
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-08-29 22:54:03 +0530

    cart and common.sh code dry

--- common.sh
+++ common.sh
@@ -135,4 +135,24 @@ func_app_prereq
 
 }
 
+func_python()
+{
+print_head "Install Python"
+yum install python36 gcc python3-devel -y &>>log_file
+func_status_check $?
 
+func_app_prereq
+
+print_head "Install Python Dependencies"
+cd /app &>>$log_file
+pip3.6 install -r requirements.txt &>>$log_file
+func_status_check $?
+
+print_head  "Setup SystemD Payment service"
+sed -i -e 's|rabbitmq_user_password|${rabbitmq_user_password}|' ${script_path}/payment.service &>>$log_file
+cp ${script_path}/payment.service /etc/systemd/system/payment.service &>>$log_file
+func_status_check $?
+
+func_systemd
+
+}
--- payment.sh
+++ payment.sh
@@ -12,24 +12,6 @@ then
 fi
 
 
-print_head "Install Python"
-yum install python36 gcc python3-devel -y &>>log_file
-func_status_check $?
+func_python
 
-func_app_prereq
-
-print_head "Install Python Dependencies"
-cd /app &>>$log_file
-pip3.6 install -r requirements.txt &>>$log_file
-func_status_check $?
-
-print_head  "Setup SystemD Payment service"
-sed -i -e 's|rabbitmq_user_password|${rabbitmq_user_password}|' ${script_path}/payment.service &>>$log_file
-cp ${script_path}/payment.service /etc/systemd/system/payment.service &>>$log_file
-func_status_check $?
-
-print_head "load and restart"
-systemctl daemon-reload &>>$log_file
-systemctl enable payment &>>$log_file
-systemctl start payment &>>$log_file
-func_status_check $?
+func_systemd
