fc4b0ed0e011c52d662262cd4900fd61f5f96426
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-08-29 14:56:39 +0530

    cart and common.sh code dry

--- mongodb.sh
+++ mongodb.sh
@@ -5,23 +5,23 @@ source ${script_path}/common.sh
 
 
 print_head "Configure mongo repo"
-cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo &>>log_file
 func_status_check $?
 
 print_head "Install Mongo software"
-yum install mongodb-org -y
-func_status_check
+yum install mongodb-org -y &>>$log_file
+func_status_check $?
 
 print_head "enable and Start mongod"
-systemctl enable mongod
-systemctl start mongod
+systemctl enable mongod &>>$log_file
+systemctl start mongod &>>$log_file
 func_status_check $?
 
 print_head " replace the IP Address "
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf &>>$log_file
 #edit the file and replace 127.0.0.1 to 0.0.0.
 func_status_check $?
 
 print_head "restart mongod "
-systemctl restart mongod
+systemctl restart mongod &>>$log_file
 func_status_check $?
