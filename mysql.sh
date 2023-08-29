f8081f0a274adfdc47f93425870d7af765b8716c
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-08-29 15:00:44 +0530

    cart and common.sh code dry

--- mysql.sh
+++ mysql.sh
@@ -10,18 +10,24 @@ then
   exit
 fi
 
-echo -e "\e[34m>>>>>>> Disbale modyule sql version 8 >>>>>>>\e[0m"
-yum module disable mysql -y
 
-echo -e "\e[34m>>>>>>> Configure Mysql repo >>>>>>>\e[0m"
-cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo
+print_head " Disbale modyule sql version 8 "
+yum module disable mysql -y &>>$log_file
+func_status_check $?
 
-echo -e "\e[34m>>>>>>> Install the mysql <<<<<<<\e[0m"
-yum install mysql-community-server -y
+print_head "Configure Mysql repo"
+cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
+func_status_check $?
 
-echo -e "\e[34m>>>>>>> start and enable MYSQl >>>>>>>\e[0m"
-systemctl enable mysqld
-systemctl start mysqld
+print_head "Install the mysql"
+yum install mysql-community-server -y &>>$log_file
+func_status_check $?
 
-echo -e "\e[34m>>>>>>> Set root pass >>>>>>>\e[0m"
-mysql_secure_installation --set-root-pass $mysql_root_password
+print_head "start and enable MYSQl"
+systemctl enable mysqld &>>$log_file
+systemctl start mysqld &>>$log_file
+func_status_check $?
+
+print_head " Set root pass "
+mysql_secure_installation --set-root-pass $mysql_root_password &>>$log_file
+func_status_check $?
